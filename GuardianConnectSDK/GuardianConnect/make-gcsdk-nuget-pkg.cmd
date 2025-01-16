e5eb8240d81f002b619760284bd7734ebdb9500b
Tim Erdies <terdies@dnsfilter.com>
2025-01-15 20:25:18 -0500

    wrong nuget command

--- GuardianConnectSDK/GuardianConnect/make-gcsdk-nuget-pkg.cmd
+++ GuardianConnectSDK/GuardianConnect/make-gcsdk-nuget-pkg.cmd
@@ -1,5 +1,5 @@
 #nuget pack GuardianConnect.nuspec
 #nuget add GuardianConnect.0.9.48.317.nupkg -source %LOCALAPPDATA%\GuardianConnectSDK\NugetPackages
 
-dotnet pack GuardianConnect.nuspec
+#dotnet pack GuardianConnect.nuspec
 dotnet add GuardianConnect.0.9.48.317.nupkg -source GuardianConnectSDK_NugetPackages
