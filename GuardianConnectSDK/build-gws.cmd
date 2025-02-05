3eb55b7d01adf30af2e8d4f9f0620931d0c90bd8
Tim Erdies <terdies@dnsfilter.com>
2025-02-05 15:49:33 -0500

    separate out the service from the sdk and build and package both

--- GuardianConnectSDK/build-gws.cmd
+++ GuardianConnectSDK/build-gws.cmd
@@ -1,2 +1,3 @@
 rem msbuild /restore /t:GuardianWinService /p:PlatformTarget=ARM64 /p:Configuration=Release /p:RuntimeIdentifer=win-ARM64 guardianwindows.sln
-msbuild /t:restore /t:Build /t:GuardianWinService /p:Platform=%1 /p:Configuration=Release /p:RuntimeIdentifier=win-%1 guardianwindows.sln
+rem msbuild /t:restore /t:Build /t:GuardianWinService /p:Platform=%1 /p:Configuration=Release /p:RuntimeIdentifier=win-%1 guardianwindows.sln
+msbuild /t:restore /t:build /p:Platform=%1 /p:Configuration=Release /p:RuntimeIdentifer=win-%1 GuardianWinService\GuardianWinService.csproj
