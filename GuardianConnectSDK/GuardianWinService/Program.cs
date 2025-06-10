494b0a31a6e4fcba663fd2a26460c71aa362b4f3
Tim Erdies <terdies@dnsfilter.com>
2025-06-10 13:51:38 -0400

    put in startup check restriction so service only runs as SYSTEM

--- GuardianConnectSDK/GuardianWinService/Program.cs
+++ GuardianConnectSDK/GuardianWinService/Program.cs
@@ -1,11 +1,22 @@
-﻿using GuardianConnect.Shared;
+﻿using System.Text;
+using GuardianConnect.Shared;
 using GuardianWinService;
 using Microsoft.Extensions.Hosting;
+using Serilog;
 
 // setup  logging first
 var paths = new[] { @"C:\temp", "GuardianFirewallService", "Diagnostics.log" };
 Common.LogFilePath = Path.Combine(paths);
 Common.SetUpLogging();
+
+// Checking if running as SYSTEM
+Log.Information($"MachineName='{Environment.MachineName}'. Username='{Environment.UserName}. User Domain Name='{Environment.UserDomainName}'. IsPrivileged = {Environment.IsPrivilegedProcess}");
+//
+if (Environment.UserName != $"{Environment.MachineName}$")
+{
+    Log.Error("*!*!*!*!*!*!*!*!*!*!*! THIS SERVICE PROCESS IS NOT EXECUTING AS SYSTEM USER. TERMINATING NOW *!*!*!*!*!*!*!*!*!*!*!*!");
+    Environment.Exit(-1);
+}
 
 PowerTransitionHandler.SetupPowerTransitionHandler();
 
