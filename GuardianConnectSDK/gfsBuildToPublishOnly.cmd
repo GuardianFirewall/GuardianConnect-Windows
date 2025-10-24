dc35f50292cff4090329e028da468aa0f06e14e4
Tim Erdies <terdies@dnsfilter.com>
2025-10-24 11:21:46 -0400

    bad msbuild switch

--- GuardianConnectSDK/gfsBuildToPublishOnly.cmd
+++ GuardianConnectSDK/gfsBuildToPublishOnly.cmd
@@ -1,5 +1,5 @@
 echo "Building Publish-AOT-Trimmed-Self-Contained GuardianFirewallService.exe"
-msbuild /t:restore /p:Build /p:Configuration=Release /p:Platform=%1 /p:WarningLevel=0 /p:RuntimeIdentifier=win-%1 GuardianFirewallService\GuardianFirewallService.csproj
+msbuild /t:restore /t:Build /p:Configuration=Release /p:Platform=%1 /p:WarningLevel=0 /p:RuntimeIdentifier=win-%1 GuardianFirewallService\GuardianFirewallService.csproj
 msbuild /t:Publish /p:PublishReadyToRun=false /p:IncludeNativeLibrariesForSelfExtract=false /p:IncludeAllContentForSelfExtract=false /p:Configuration=Release /p:Platform=%1 /p:WarningLevel=0 /p:RuntimeIdentifier=win-%1 GuardianFirewallService\GuardianFirewallService.csproj
 
 :exit
